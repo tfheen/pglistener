5dea7e8327c9491088ef30fa206fccab0f7f2dff
pglistener <pglistener@light.bluelinux.co.uk>
2007-10-20 19:43:53 +0200

    make the starter fork off each config section in a seperate process
    darcs-hash:20071020174353-80993-2b82692eb8e5a4248f131dda45b943f110f6a29b.gz

--- starter.py
+++ starter.py
@@ -3,32 +3,36 @@
 from ConfigParser import SafeConfigParser
 
 import sys,os
+import thread
 
 sys.path.append("src")
 
 cf = SafeConfigParser()
 
 configfile=sys.argv[1]
-section=sys.argv[2]
 
 if (not os.path.exists(configfile)):
   print "ERROR: File not found: %s" % configfile
 cf.read(configfile)
 
-if section:
-  
+for section in cf.sections():
+
+  print section
+
   # Import the appropriate class
   # Module name = lower of class name
   classname = cf.get(section,"class")
 
-  if (not os.path.exists(classname.lower() + ".py")):
-    print "ERROR: Module not found: %s" % classname
   exec("from %s import %s" % (classname.lower(),classname))
   options=dict(cf.items(section))
   print options
   options['notifications']=eval(options['notifications'])
   options['posthooks']=eval(options['posthooks'])
   options['name']=section
-  listener = eval("%s(%s)" % (classname,options))
+  pid = os.fork()
 
+  if pid == 0:
+    pid = os.fork()
+    if pid == 0:
+      listener = eval("%s(%s)" % (classname,options))
 
