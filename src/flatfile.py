e1ddb82faf17c2296d281617c357e87773ee01a9
pglistener <pglistener@light.bluelinux.co.uk>
2007-10-20 19:44:39 +0200

    make tab the default flat file delimiter
    darcs-hash:20071020174439-80993-611902a4a3250fd4229fe81446ad86e17988ddfd.gz

--- src/flatfile.py
+++ src/flatfile.py
@@ -3,7 +3,7 @@ from pglistener import PgListener
 class FlatFile(PgListener):
   def __init__(self,options):
     if(not options.has_key('delimeter')):
-      options['delimeter']=' '
+      options['delimeter']='\t'
     PgListener.__init__(self,options)
 
   def do_format(self,row):
