8c33d467156bdc0820fdb67d9135e90cdd0eaaf5
Robert McQueen <robert.mcqueen@bluelinux.co.uk>
2008-11-04 18:55:30 +0100

    totally untested and probably broken patch to daemonise, unpull this if stuff fails
    darcs-hash:20081104175530-be372-376605f76d6a52092e699cc8e56468435a0a1a30.gz

--- starter.py
+++ starter.py
@@ -5,6 +5,47 @@ from ConfigParser import SafeConfigParser
 import sys,os
 import thread
 
+def createDaemon():
+  pid = os.fork()
+
+  # exit parent
+  if pid != 0:
+    os._exit(0)
+
+  # become session leader
+  os.setsid()
+
+  pid = os.fork()
+
+  # exit first child
+  if pid != 0:
+    os._exit(0)
+
+  return
+
+if 0:
+  # find max number of FDs
+  if (os.sysconf_names.has_key("SC_OPEN_MAX")):
+    maxfd = os.sysconf("SC_OPEN_MAX")
+  else:
+    maxfd = 1024
+
+  # close all FDs
+  for fd in range(0, maxfd):
+    try:
+      os.close(fd)
+    except OSError:
+      pass
+
+  if (hasattr(os, "devnull")):
+    redirect_to = os.devnull
+  else:
+    redirect_to = "/dev/null"
+
+  os.open(redirect_do, os.O_RDWR)
+  os.dup2(0, 1)
+  os.dup2(0, 2)
+
 sys.path.append("src")
 
 cf = SafeConfigParser()
@@ -12,8 +53,11 @@ cf = SafeConfigParser()
 configfile=sys.argv[1]
 
 if (not os.path.exists(configfile)):
-  print "ERROR: File not found: %s" % configfile
+  os.exit("ERROR: File not found: %s" % configfile)
+
 cf.read(configfile)
+
+createDaemon()
 
 for section in cf.sections():
 
@@ -29,8 +73,8 @@ for section in cf.sections():
   options['notifications']=eval(options['notifications'])
   options['posthooks']=eval(options['posthooks'])
   options['name']=section
-  pid = os.fork()
 
+  pid = os.fork()
   if pid == 0:
     listener = eval("%s(%s)" % (classname,options))
     break
