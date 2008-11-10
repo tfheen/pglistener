46777c2bf8390dcde98c18aaea5060d34c738078
Dafydd Harries <dafydd.harries@collabora.co.uk>
2008-11-10 20:56:07 +0100

    convert starter to four-space indent
    darcs-hash:20081110195607-c9803-8504f340eb1ef39dfc57558f054f7474cf3f3476.gz

--- starter.py
+++ starter.py
@@ -24,83 +24,83 @@ import sys,os
 import thread
 
 def createDaemon():
-  pid = os.fork()
+    pid = os.fork()
 
-  # exit parent
-  if pid != 0:
-    os._exit(0)
+    # exit parent
+    if pid != 0:
+        os._exit(0)
 
-  # become session leader
-  os.setsid()
-
-  pid = os.fork()
-
-  # exit first child
-  if pid != 0:
-    os._exit(0)
-
-  return
-
-if 0:
-  # find max number of FDs
-  if (os.sysconf_names.has_key("SC_OPEN_MAX")):
-    maxfd = os.sysconf("SC_OPEN_MAX")
-  else:
-    maxfd = 1024
-
-  # close all FDs
-  for fd in range(0, maxfd):
-    try:
-      os.close(fd)
-    except OSError:
-      pass
-
-  if (hasattr(os, "devnull")):
-    redirect_to = os.devnull
-  else:
-    redirect_to = "/dev/null"
-
-  os.open(redirect_do, os.O_RDWR)
-  os.dup2(0, 1)
-  os.dup2(0, 2)
-
-def main(argv):
-  sys.path.append("src")
-
-  cf = SafeConfigParser()
-
-  configfile=sys.argv[1]
-
-  if (not os.path.exists(configfile)):
-    print "ERROR: File not found: %s" % configfile
-    return 1
-
-  cf.read(configfile)
-
-  createDaemon()
-
-  for section in cf.sections():
-
-    print section
-
-    # Import the appropriate class
-    # Module name = lower of class name
-    classname = cf.get(section,"class")
-
-    exec("from %s import %s" % (classname.lower(),classname))
-    options=dict(cf.items(section))
-    print options
-    options['notifications']=eval(options['notifications'])
-    options['posthooks']=eval(options['posthooks'])
-    options['name']=section
+    # become session leader
+    os.setsid()
 
     pid = os.fork()
-    if pid == 0:
-      listener = eval("%s(%s)" % (classname,options))
-      break
 
-  return 0
+    # exit first child
+    if pid != 0:
+        os._exit(0)
+
+    return
+
+if 0:
+    # find max number of FDs
+    if (os.sysconf_names.has_key("SC_OPEN_MAX")):
+        maxfd = os.sysconf("SC_OPEN_MAX")
+    else:
+        maxfd = 1024
+
+    # close all FDs
+    for fd in range(0, maxfd):
+        try:
+            os.close(fd)
+        except OSError:
+            pass
+
+    if (hasattr(os, "devnull")):
+        redirect_to = os.devnull
+    else:
+        redirect_to = "/dev/null"
+
+    os.open(redirect_do, os.O_RDWR)
+    os.dup2(0, 1)
+    os.dup2(0, 2)
+
+def main(argv):
+    sys.path.append("src")
+
+    cf = SafeConfigParser()
+
+    configfile=sys.argv[1]
+
+    if (not os.path.exists(configfile)):
+        print "ERROR: File not found: %s" % configfile
+        return 1
+
+    cf.read(configfile)
+
+    createDaemon()
+
+    for section in cf.sections():
+
+        print section
+
+        # Import the appropriate class
+        # Module name = lower of class name
+        classname = cf.get(section,"class")
+
+        exec("from %s import %s" % (classname.lower(),classname))
+        options=dict(cf.items(section))
+        print options
+        options['notifications']=eval(options['notifications'])
+        options['posthooks']=eval(options['posthooks'])
+        options['name']=section
+
+        pid = os.fork()
+        if pid == 0:
+            listener = eval("%s(%s)" % (classname,options))
+            break
+
+    return 0
 
 if __name__ == '__main__':
-  sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv))
 
