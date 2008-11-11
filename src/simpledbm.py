5951da605331918c1f50f60afa0ddb08b77523c2
Dafydd Harries <dafydd.harries@collabora.co.uk>
2008-11-11 14:48:14 +0100

    remove unused import
    darcs-hash:20081111134814-c9803-45487455518e7ce94a0dba6af7908c32cc646278.gz

--- src/simpledbm.py
+++ src/simpledbm.py
@@ -16,7 +16,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import sys, psycopg
+import sys
 from pglistener import PgListener
 import select, time, os, signal, errno, stat
 from syslog import *
