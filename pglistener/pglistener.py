cd07e695334f275b0acac849a31f213554317359
Dafydd Harries <dafydd.harries@collabora.co.uk>
2008-12-15 14:55:50 +0100

    remove unused code
    darcs-hash:20081215135550-c9803-fdcdbc5d3fe18c9a77139949c07e57fd1654ce40.gz

--- pglistener/pglistener.py
+++ pglistener/pglistener.py
@@ -21,9 +21,6 @@ import os
 import select
 import stat
 import syslog
-import time
-
-import psycopg2
 
 class PgListener:
     def __init__(self, options):
@@ -38,44 +35,8 @@ class PgListener:
         self.destination = options['destination']
         self.conn = None
 
-    def try_connect(self):
-        if self.conn:
-            self.conn.close()
-
-        self.conn = psycopg2.connect(self.dsn)
-        self.conn.set_isolation_level(0)
-        self.cursor = self.conn.cursor()
-
-    def connect(self):
-        try:
-            self.try_connect()
-        except psycopg2.DatabaseError, e:
-            self.log(syslog.LOG_ERR,
-                "Exception: %s. Reconnecting and retrying." % e)
-
-            # Exponential backoff foo
-            if self.sleeptime == 0:
-                self.sleeptime = 1
-            else:
-                time.sleep(self.sleeptime)
-                self.sleeptime = self.sleeptime * 2
-
-            self.connect()
-            self.sleeptime = 0
-
     def log(self, priority, msg):
         syslog.syslog(priority, "%s: %s" % (self.name, msg))
-
-    def do_query(self):
-        """Execute the query supplied returning all the rows."""
-        try:
-            self.cursor.execute(self.query)
-            return self.cursor.fetchall()
-        except psycopg2.DatabaseError, e:
-            self.log(syslog.LOG_ERR,
-                "Exception: %s. Reconnecting and retrying." % e)
-            self.connect()
-            self.do_query()
 
     def do_format(self, row):
         """Apply the format string against a row."""
@@ -116,40 +77,10 @@ class PgListener:
                 self.log(syslog.LOG_ERR,
                     "Failed to chmod new file: %s" % strerror)
 
-    def do_update(self):
-        """Update the destination file with data from the database."""
-
-        result = self.do_query()
-        self.log(syslog.LOG_NOTICE, "Updating: %s" % self.destination)
-        self.do_write(result, self.destination)
-
     def do_posthooks(self):
         """Execute all the provided hooks."""
 
         for hook in self.options.get('posthooks', '').strip().splitlines():
             self.log(syslog.LOG_INFO, "Executing: %s" % hook)
             os.system(hook)
-
-    def get_notifies(self):
-        """Get any pending notifications."""
-
-        try:
-            if not self.cursor.isready():
-                return []
-
-            notifies = self.conn.notifies[:]
-            self.conn.notifies[:] = []
-            return [name for pid, name in notifies]
-        except psycopg2.DatabaseError, e:
-            self.log(syslog.LOG_ERR,
-                "Exception: %s. Reconnecting and retrying." % e)
-            self.connect()
-            self.get_notifies()
-
-    def listen(self):
-        # Set up the appropriate notifications
-
-        for n in self.notifications:
-            self.log(syslog.LOG_INFO, "Listening for: %s" % n)
-            self.cursor.execute("listen \"%s\"" % n)
 
