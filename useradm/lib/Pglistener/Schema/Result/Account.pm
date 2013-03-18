0180edb7b7b7ee9319959f0cb1e4f1995827c514
Tollef Fog Heen <tfheen@err.no>
2013-03-18 13:13:27 +0100

    Add email field and handling

--- useradm/lib/Pglistener/Schema/Result/Account.pm
+++ useradm/lib/Pglistener/Schema/Result/Account.pm
@@ -2,7 +2,7 @@ package Pglistener::Schema::Result::Account;
 use base qw/DBIx::Class::Core/;
 
 __PACKAGE__->table('account');
-__PACKAGE__->add_columns(qw/ account_id username name shell password enabled gpg_key /);
+__PACKAGE__->add_columns(qw/ account_id username name shell password enabled gpg_key email/);
 __PACKAGE__->set_primary_key('account_id');
 __PACKAGE__->has_many(sshkeys => 'Pglistener::Schema::Result::SSHKey', "account_id" );
 __PACKAGE__->has_many(account_grp => 'Pglistener::Schema::Result::AccountGrp', "account_id" );
