85b8211d00fc7f51cd598146cc0da587d44763aa
Tollef Fog Heen <tfheen@err.no>
2015-03-18 14:53:18 +0100

    Re-add email class for setups that have this in a separate table

--- useradm/lib/Pglistener/Schema/Result/Account.pm
+++ useradm/lib/Pglistener/Schema/Result/Account.pm
@@ -7,5 +7,6 @@ __PACKAGE__->set_primary_key('account_id');
 __PACKAGE__->has_many(sshkeys => 'Pglistener::Schema::Result::SSHKey', "account_id" );
 __PACKAGE__->has_many(account_grp => 'Pglistener::Schema::Result::AccountGrp', "account_id" );
 __PACKAGE__->many_to_many(groups => 'account_grp', 'grp');
+__PACKAGE__->has_many(emails => 'Pglistener::Schema::Result::Email', "account_id" );
 
 1;
